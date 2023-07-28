d11d608de14d4641d46affc2f7141c333bd31927
David <osodavid001@gmail.com>
2023-07-28 22:55:22 +0100

    created update user response

--- DiaryApp/src/main/java/com/diary/DiaryApp/services/user/UserService.java
+++ DiaryApp/src/main/java/com/diary/DiaryApp/services/user/UserService.java
@@ -1,10 +1,12 @@
 package com.diary.DiaryApp.services.user;
 
 import com.diary.DiaryApp.data.dto.request.RegisterUserRequest;
+import com.diary.DiaryApp.data.dto.request.UpdateUserRequest;
 import com.diary.DiaryApp.data.dto.request.UploadImageRequest;
 import com.diary.DiaryApp.data.dto.request.UserLoginRequest;
 import com.diary.DiaryApp.data.dto.response.RegisterUserResponse;
 import com.diary.DiaryApp.data.dto.response.OtpVerificationResponse;
+import com.diary.DiaryApp.data.dto.response.UpdateUserResponse;
 import com.diary.DiaryApp.data.dto.response.UserLoginResponse;
 import com.diary.DiaryApp.data.model.User;
 
