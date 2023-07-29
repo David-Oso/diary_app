ac601fb217ea7b2941c0b32ccfa06a76978e31e1
David <osodavid001@gmail.com>
2023-07-29 10:49:19 +0100

    entry service created create entry request

--- DiaryApp/src/main/java/com/diary/DiaryApp/services/entry/EntryService.java
+++ DiaryApp/src/main/java/com/diary/DiaryApp/services/entry/EntryService.java
@@ -1,4 +1,7 @@
 package com.diary.DiaryApp.services.entry;
 
+import com.diary.DiaryApp.data.dto.response.CreateEntryResponse;
+
 public interface EntryService {
+    CreateEntryResponse createEntry()
 }
