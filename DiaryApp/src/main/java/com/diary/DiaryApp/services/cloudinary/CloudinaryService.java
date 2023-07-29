ed7355cca1699bb44de899e19a9e544131845d03
David <osodavid001@gmail.com>
2023-07-29 02:36:30 +0100

    added a cloudinary bean to app config

--- DiaryApp/src/main/java/com/diary/DiaryApp/services/cloudinary/CloudinaryService.java
+++ DiaryApp/src/main/java/com/diary/DiaryApp/services/cloudinary/CloudinaryService.java
@@ -1,4 +1,7 @@
 package com.diary.DiaryApp.services.cloudinary;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface CloudinaryService {
+    String upload(MultipartFile image);
 }
---
+++ DiaryApp/src/main/java/com/diary/DiaryApp/services/cloudinary/CloudinaryServiceImpl.java
@@ -0,0 +1,29 @@
+package com.diary.DiaryApp.services.cloudinary;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import com.diary.DiaryApp.exception.ImageUploadException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class CloudinaryServiceImpl implements CloudinaryService{
+    private final Cloudinary cloudinary;
+    @Override
+    public String upload(MultipartFile image) {
+        try{
+            Map<?, ?> response = cloudinary.uploader()
+                    .upload(image.getBytes(), ObjectUtils.emptyMap());
+            return response.get("url").toString();
+        }catch (IOException exception){
+            throw new ImageUploadException(exception.getMessage());
+        }
+    }
+}
