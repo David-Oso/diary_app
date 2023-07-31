497fb0d76a3e298273b3e0b5de835d49d56c4e6b
David <osodavid001@gmail.com>
2023-07-31 09:08:45 +0100

    created auth controller

---
+++ DiaryApp/src/main/java/com/diary/DiaryApp/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.diary.DiaryApp.controller;
+
+import com.diary.DiaryApp.services.user.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@AllArgsConstructor
+public class AuthController {
+    private final UserService userService;
+
+}
