3a9e074ee1d905cc43731aed20fd9fb45f430ba4
David <osodavid001@gmail.com>
2023-07-31 11:15:08 +0100

    validated create entry request class

--- DiaryApp/src/main/java/com/diary/DiaryApp/data/dto/request/CreateEntryRequest.java
+++ DiaryApp/src/main/java/com/diary/DiaryApp/data/dto/request/CreateEntryRequest.java
@@ -1,5 +1,8 @@
 package com.diary.DiaryApp.data.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateEntryRequest {
+    @NotNull(message = "field user id cannot be null")
+
     private Long userId;
+    @NotNull(message = "field title cannot be null")
+    @NotBlank(message = "field title cannot be blank")
+    @NotEmpty(message = "field title cannot be empty")
     private String title;
+
+    @NotNull(message = "field body cannot be null")
+    @NotBlank(message = "field body cannot be blank")
+    @NotEmpty(message = "field body cannot be empty")
     private String body;
 }
