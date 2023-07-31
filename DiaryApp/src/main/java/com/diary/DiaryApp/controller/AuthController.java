b39ece389ba2f28775158d3f6b933106a994d368
David <osodavid001@gmail.com>
2023-07-31 09:40:12 +0100

    validated register request

--- DiaryApp/src/main/java/com/diary/DiaryApp/controller/AuthController.java
+++ DiaryApp/src/main/java/com/diary/DiaryApp/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.diary.DiaryApp.controller;
 
 import com.diary.DiaryApp.services.user.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final UserService userService;
 
-
 }
+
+
+
+//
+//@PostMapping(value="upload_profile_image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//@Operation(summary = "login", description = "authenticating registered user")
+//public ResponseEntity<?> uploadProfileImage(@Valid @ModelAttribute UploadImageRequest uploadImageRequest){
+//    String response = userService.uploadProfileImage(uploadImageRequest);
+//    return ResponseEntity.ok(response);
+//}
--- DiaryApp/src/main/java/com/diary/DiaryApp/data/dto/request/RegisterUserRequest.java
+++ DiaryApp/src/main/java/com/diary/DiaryApp/data/dto/request/RegisterUserRequest.java
@@ -1,17 +1,34 @@
 package com.diary.DiaryApp.data.dto.request;
 
-import com.diary.DiaryApp.data.model.User;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import static com.diary.DiaryApp.utilities.ValidationUtils.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class RegisterUserRequest {
+    @NotNull(message = "field user name cannot be null")
+    @NotBlank(message = "field user name cannot be blank")
+    @NotEmpty(message = "field user name cannot be empty")
+    @Pattern(regexp = NAME_REGEX, message = "user name must be letter starting with capital letter")
     private String userName;
+
+    @NotNull(message = "field email cannot be null")
+    @NotBlank(message = "field email cannot be blank")
+    @NotEmpty(message = "field email cannot be empty")
+    @Email(regexp = EMAIL_REGEX, message = "enter a valid email address")
     private String email;
+
+    @NotNull(message = "field password cannot be null")
+    @NotBlank(message = "field password cannot be blank")
+    @NotEmpty(message = "field password cannot be empty")
+    @Pattern(regexp = PASSWORD_REGEX, message = "Password must " +
+            "contain at least one capital letter, one small letter, a number and special character(@$!%*?&)")
     private String password;
 }
