11552d3f5f32146a90be37858b6943933f6ee0c6
David <osodavid001@gmail.com>
2023-10-02 19:45:06 +0100

    passw

--- DiaryApp/src/main/java/com/diary/DiaryApp/data/dto/request/RegisterUserRequest.java
+++ DiaryApp/src/main/java/com/diary/DiaryApp/data/dto/request/RegisterUserRequest.java
@@ -27,7 +27,6 @@ public class RegisterUserRequest {
 
     @NotNull(message = "field password cannot be null")
     @NotBlank(message = "field password cannot be blank")
-
     @NotEmpty(message = "field password cannot be empty")
     @Pattern(regexp = PASSWORD_REGEX, message = "Password must " +
             "contain at least one capital letter, one small letter, a number and special character(@$!%*?&)")
