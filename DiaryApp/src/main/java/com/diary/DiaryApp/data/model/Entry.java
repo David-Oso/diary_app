78471b2c12e1882701d555808714ce9bb2dd5b3b
David <osodavid001@gmail.com>
2023-10-03 07:11:32 +0100

    g

--- DiaryApp/src/main/java/com/diary/DiaryApp/data/model/Entry.java
+++ DiaryApp/src/main/java/com/diary/DiaryApp/data/model/Entry.java
@@ -20,6 +20,7 @@ public class Entry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String description;
     private String body;
